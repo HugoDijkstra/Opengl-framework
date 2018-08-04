6f3b950f4c744ba42ffbd3c3e484437cc74b3333
Hugo Dijkstra <hugodijkstra@live.nl>
2018-08-04 18:11:05 +0200

    Fixed calling temporary object in main

--- game/main.cpp
+++ game/main.cpp
@@ -6,9 +6,9 @@ int main()
 	Renderer* renderer = new Renderer();
 	Mesh* mesh = new Mesh();
 	renderer->CreateShader("Default", "shaders/vertex.vert", "shaders/frag.frag");
-	Shader* s = &renderer->GetShader("Default");
-	s->Use();
-	s->SetVector3("ColorIn", 0, 1, 0);
+	Shader s = renderer->GetShader("Default");
+	s.Use();
+	s.SetVector3("ColorIn", 1, 1, 0);
 	do {
 		renderer->RenderMesh(mesh, glm::mat4());
 		renderer->SwapBuffer();
